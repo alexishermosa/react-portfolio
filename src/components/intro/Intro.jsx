8f3a2a9e6a4d65920ff9d5a6afb9bcd4e64419ee
alexishermosa <alexishermosa@hotmail.com>
2022-09-08 20:09:32 -0300

    crear introduccion, imagen

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -3,6 +3,13 @@ import './Intro.css'
 import Github from '../../img/github.png'
 import Linkedin from '../../img/linkedin.png'
 import Instagram from '../../img/instagram.png'
+import Vector1 from '../../img/Vector1.png'
+import Vector2 from '../../img/Vector2.png'
+import boy from '../../img/boy.png'
+import thumbup from '../../img/thumbup.png'
+import Crown from '../../img/crown.png'
+import glassesimoji from '../../img/glassesimoji.png'
+import { FloatingDiv } from '../floatingdiv/FloatingDiv'
 
 const Intro = () => {
   return (
@@ -25,7 +32,29 @@ const Intro = () => {
       </div>
 
       <div className="i-right">
-        Right side
+        <img src={Vector1} alt="" />
+        <img src={Vector2} alt="" />
+        <img src={boy} alt="" />
+        <img src={glassesimoji} alt="" />
+        <div className='i-crown'>
+          <FloatingDiv image={Crown} text1='Web' text2='Developer'/>
+        </div>
+        <div className='i-design'>
+          <FloatingDiv image={thumbup} text1='Best Quality' text2='Work'/>
+        </div>
+        {/*blur divs*/}
+        <div className="blur" style={{
+          background: "rgb(238 210 255)",
+          left: '18rem',
+          }} ></div>
+        <div className="blur" style={{
+          background: '#C1F5FF',
+          top: '15rem',
+          left: '-12rem',
+          width: '21rem',
+          height: '11rem'
+
+        }} ></div>
       </div>
     </div>
   )
